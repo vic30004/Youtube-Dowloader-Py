59fb0de4fc0612d5109e30c5c4a008a1320a484b
vic30004 <victor_993@msn.com>
2020-02-02 18:51:37 -0800

    created a file to open directory

--- app.py
+++ app.py
@@ -8,19 +8,30 @@ root = Tk()
 root.title('Youtube Downloader')
 root.configure(background="#333")
 root.minsize(width=420, height=100)
+
+# Main Functions 
+def open_file():
+    global directory
+    directory = askdirectory()
+    print(directory)
+
+
 # this wont let user change the size of the widget
 root.resizable(width=False, height=False)
 
+# Link Label
 link_lbl = Label(root, text="Enter Link",
                  font="Helvetica 12 bold").grid(row=1, column=0)
-
+#Link Input
 link = StringVar()
 link_entry = Entry(root, textvariable=link, width=50,
                    borderwidth=4).grid(row=1, column=1)
 
+#Select directory Buton 
 select_dir = Button(root, text="Choose Directory", bg="#DC143C", font="Helvetica 10 bold", width=15,
-                    command='').grid(row=1, column=2)
+                    command=open_file).grid(row=1, column=2)
 
+# Download Button
 download_btn = Button(root, text="Download", bg="light green",font="Helvetica 10 bold", width=10,
                       command='').grid(row=2, column=1)
 
