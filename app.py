7fb07ae9f00161925e64a1c5cf52bf1d8d905bc8
vic30004 <victor_993@msn.com>
2020-02-02 18:48:38 -0800

    minor style updates

--- app.py
+++ app.py
@@ -12,14 +12,16 @@ root.minsize(width=420, height=100)
 root.resizable(width=False, height=False)
 
 link_lbl = Label(root, text="Enter Link",
-                 font="Helvetica 15 bold").grid(row=1, column=0)
+                 font="Helvetica 12 bold").grid(row=1, column=0)
 
 link = StringVar()
 link_entry = Entry(root, textvariable=link, width=50,
                    borderwidth=4).grid(row=1, column=1)
 
-select_dir = Button(root, text="Choose Directory", width=15,
+select_dir = Button(root, text="Choose Directory", bg="#DC143C", font="Helvetica 10 bold", width=15,
                     command='').grid(row=1, column=2)
 
-download_btn = Button(root, text="Download", width=10,
+download_btn = Button(root, text="Download", bg="light green",font="Helvetica 10 bold", width=10,
                       command='').grid(row=2, column=1)
+
+root.mainloop()
