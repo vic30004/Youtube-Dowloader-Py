959a2fafc20ef627bab639564549e28b52e3a098
vic30004 <victor_993@msn.com>
2020-02-02 18:34:05 -0800

    added code for the interface

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from tkinter import TK(),Button,Lable,StringVar,Entry
+from tkinter import Tk(), Button, Lable, StringVar, Entry
 from tkinter.filedialog import askdirectory
 from tkinter import messagebox
-from tkinter import YouTube 
+from tkinter import YouTube
+
+# This is the interface 
+root = Tk()
+root.title('Youtube Downloader')
+root.configure(background="#333")
+root.minsize(width=420, height=100)
+root.resizable(width=False, height=False) #this wont let user change the size of the widget 
